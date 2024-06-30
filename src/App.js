16dfb526f467b114e1b4eefe7c99e9e51af6d4d2
Lee ChangRyoel <dlckdfuf141@naver.com>
2024-06-30 23:58:23 +0900

    게시판 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Home from './component/Home';
 import Login from './component/Login';
 import Register from './component/Register';
 import MainPage from './component/MainPage';
+import WritePost from './component/post/WritePost';
+import MyPost from './component/post/MyPost';
+import AllPost from './component/post/AllPost';
+import PostDetails from './component/post/PostDetails';
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path='/home' element={<MainPage />}/>
+        <Route path='/writepost' element={<WritePost />} />
+        <Route path='/mypost' element={<MyPost />}/>
+        <Route path='/allpost' element={<AllPost />} />
+        <Route path='/post/:id' element={<PostDetails />} />
       </Routes>
     </Router> 
   );
