42796c2d17068f4ee4aee4fa31bf839e22454f05
Lee ChangRyoel <dlckdfuf141@naver.com>
2024-07-01 00:00:30 +0900

    내 게시글 확인 기능 추가

---
+++ src/component/post/MyPost.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { db } from '../../firebase';
+
+function MyPost() {
+    const [user, setUser] = useState(null);
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        //현재 사용자 불러옴
+        const auth = getAuth();
+        //현재 사용자 상태를 감지하고 'user'상태를 설정
+        //auth = Firebase Authentication 인스턴스
+        //콜백함수: currentUser를 인자로 받음
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            //사용자가 존재하면 setUser를 사용해 user업데이트하고, fetchUserPosts함수 호출하고
+            //인자에 현재 사용자 이메일(아이디)를 넣음
+            if (currentUser) {
+                setUser(currentUser);
+                fetchUserPosts(currentUser.email);
+            //사용자를 찾을 수없다면 user를 초기화하고 post를 빈배열로 설정
+            //사용자가 로그아웃하면 null로 처리
+            } else {
+                setUser(null);
+                setPosts([]);
+            }
+        });
+        //메모리 누수 방지 및 불필요한 이벤트 리스너가 남지 않도록함.
+        return () => unsubscribe();
+        //useEffect의 두번 째 인자인 의존성 배열인 []는 []안의 값이 변경될때마다
+        //useEffect 훅이 다시 실행되도록함, 하지만 일반적으로 []안의 값은 변경되지 않아
+        //useEffect 훅은 컴포넌트가 처음 마운트 될때 한번만 실행됨.
+    }, []);
+
+
+    const fetchUserPosts = async (userEmail) => {
+        //qurery함수는 Firesotre의 콜랙션 함수와 where조건을 사용해서 특정 조건에 맞는
+        //문서를 검색하는 쿼리를 생성(board컬랙션에서 author필드가 userEmail과 동일한 문서 찾음)
+        const q = query(collection(db, 'board'), where('author', '==', userEmail));
+        //getDocs는 주어진 쿼리를 실행해서 해당 조건에 맞는 문서의 스냅샵을 가져옴
+        const querySnapshot = await getDocs(q);
+        //querySnapshot에서 문서를 순회하여 각 문서의 id와 데이터를 추출하고, 이를 배열로 변환해
+        //userPosts에 저장함, 그 후 setPosts함수를 사용하여 posts 상태 업데이트
+        const userPosts = querySnapshot.docs.map(doc => ({
+            //doc.data에는 id값이 없으므로 userPosts에서  id를 추가하는 것임! (... <-)
+            id: doc.id, //문서의 id를 포함
+            ...doc.data() //문서의 모든 데이터 포함
+        }));
+        setPosts(userPosts);
+    };
+
+    return (
+        <div>
+            <h1>내 게시글</h1>
+            {posts.length === 0 ? (
+                <p>게시글이 없습니다.</p>
+            ) : (
+                <ul>
+                    {posts.map(post => (
+                        <li key={post.id}>
+                            <h2>{post.id}</h2> {/* 제목을 표시하려면 post.id 대신 post.title 사용 */}
+                            <p>{post.content}</p>
+                            <p>{post.time}</p>
+                            <p>{post.author}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
+
+export default MyPost;
