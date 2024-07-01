b34df1c703cb3c45c1ba4ecdb65ee69d50eacfa6
Lee ChangRyoel <dlckdfuf141@naver.com>
2024-07-01 14:22:36 +0900

    firebase 정보

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA_wktnFvtXzXLgnHTgL5pgbdPFQcPTPzI",
-    authDomain: "react-d1aea.firebaseapp.com",
-    projectId: "react-d1aea",
-    storageBucket: "react-d1aea.appspot.com",
-    messagingSenderId: "560016855921",
-    appId: "1:560016855921:web:e3f277ae940235dcc91cd0",
-    measurementId: "G-26WRREC7QX"
+    apiKey: YOUR_API_KEY,
+    authDomain: YOUR_AUTH_DOMAIN,
+    projectId: YOUR_PROJECT_ID,
+    storageBucket: YOUR_STORAGE_BUCKET,
+    messagingSenderId: YOUR_MESSAGING_SENDER_ID,
+    appId: YOUR_APP_ID,
+    measurementId: YOUR_MEASUREMENT_ID
 };
 
 // Initialize Firebase
