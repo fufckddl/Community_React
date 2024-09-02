ef24cb08595c6df1403d5b809d4e3bad3de23a53
Lee ChangRyoel <dlckdfuf141@naver.com>
2024-09-02 16:52:34 +0900

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,21 +2,23 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: YOUR_API_KEY,
-    authDomain: YOUR_AUTH_DOMAIN,
-    projectId: YOUR_PROJECT_ID,
-    storageBucket: YOUR_STORAGE_BUCKET,
-    messagingSenderId: YOUR_MESSAGING_SENDER_ID,
-    appId: YOUR_APP_ID,
-    measurementId: YOUR_MEASUREMENT_ID
+    apiKey: "AIzaSyA_wktnFvtXzXLgnHTgL5pgbdPFQcPTPzI",
+    authDomain: "react-d1aea.firebaseapp.com",
+    projectId: "react-d1aea",
+    storageBucket: "react-d1aea.appspot.com",
+    messagingSenderId: "560016855921",
+    appId: "1:560016855921:web:e3f277ae940235dcc91cd0",
+    measurementId: "G-26WRREC7QX"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-export { db, auth };
+export { db, auth, app, storage };
